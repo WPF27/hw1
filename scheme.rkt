f04ed05831b15299a5113ec2e8c1d6c1985d17e6
Geoffrey Mainland <mainland@apeiron.net>
2021-01-11 08:38:29 -0500

    Initial import.

---
+++ scheme.rkt
@@ -0,0 +1,39 @@
+;;
+;; DO NOT REMOVE THESE TWO LINES
+;;
+#lang racket
+(provide (all-defined-out))
+
+;;
+;; Problem 1
+;;
+
+(define (sigma m n)
+  'not-implemented)
+;;
+;; Problem 2
+;;
+
+(define (log m n)
+  'not-implemented)
+
+;;
+;; Problem 3
+;;
+
+(define (choose n k)
+  'not-implemented)
+
+;;
+;; Problem 4
+;;
+
+(define (binary n)
+  'not-implemented)
+
+;;
+;; Problem 5
+;;
+
+(define (scan f z l)
+  'not-implemented)
